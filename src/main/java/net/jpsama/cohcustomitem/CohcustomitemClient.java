9a3272b24342d1e84b3beee67816482cd54aed76
jpwanabe <30207571+jpwanabe@users.noreply.github.com>
2025-02-11 22:02:39 -0500

    first commit

---
+++ src/main/java/net/jpsama/cohcustomitem/CohcustomitemClient.java
@@ -0,0 +1,10 @@
+package net.jpsama.cohcustomitem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CohcustomitemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
