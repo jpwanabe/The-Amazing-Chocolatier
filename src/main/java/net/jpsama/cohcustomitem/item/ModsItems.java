977731220997e28dfd8ee562a5cf3765d2a77c65
jpwanabe <30207571+jpwanabe@users.noreply.github.com>
2025-02-12 13:41:36 -0500

    .

---
+++ src/main/java/net/jpsama/cohcustomitem/item/ModsItems.java
@@ -0,0 +1,33 @@
+package net.jpsama.cohcustomitem.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.jpsama.cohcustomitem.Cohcustomitem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import javax.xml.namespace.QName;
+
+public class ModsItems {
+    public static final Item BUBBY_BAR = registerItem("bubby_bar", new Item(new FabricItemSettings().food(ModFoodComponents.BUBBY_BAR)));
+    public static final Item BUBBY_WRAPPER = registerItem("bubby_wrapper", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(BUBBY_BAR);
+        entries.add(BUBBY_WRAPPER);
+    }
+
+
+    private static Item registerItem(String name, Item item){
+    return Registry.register(Registries.ITEM, new Identifier(Cohcustomitem.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        Cohcustomitem.LOGGER.info("Registering Mod Items for " + Cohcustomitem.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModsItems::addItemsToIngredientItemGroup);
+    }
+
+}
